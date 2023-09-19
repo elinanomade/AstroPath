62e040de7d1e65206c0337b562b8089a90cde4ef
elinanomade <elinanomad@gmail.com>
2023-09-19 11:35:40 +0200

    testimonial

---
+++ mainPage/js/main.js
@@ -0,0 +1,87 @@
+// const prevButton = document.querySelector(".prev-button");
+// const nextButton = document.querySelector(".next-button");
+// const carouselItems = document.querySelector(".carousel-items");
+// const carouselItemWidth = document.querySelector(".carousel-item").clientWidth;
+// let currentPosition = 0;
+
+// prevButton.addEventListener("click", () => {
+//   if (currentPosition < 0) {
+//     currentPosition += carouselItemWidth;
+//     carouselItems.style.transform = `translateX(${currentPosition}px)`;
+//   }
+// });
+
+// nextButton.addEventListener("click", () => {
+//   if (currentPosition > -carouselItemWidth * 7) {
+//     currentPosition -= carouselItemWidth;
+//     carouselItems.style.transform = `translateX(${currentPosition}px)`;
+//   }
+// });
+"use strict";
+
+var slidePosition = 1;
+SlideShow(slidePosition);
+
+// forward/Back controls
+function plusSlides(n) {
+  SlideShow((slidePosition += n));
+}
+
+//  images controls
+function currentSlide(n) {
+  SlideShow((slidePosition = n));
+}
+
+function SlideShow(n) {
+  var i;
+  var slides = document.getElementsByClassName("Containers");
+  var circles = document.getElementsByClassName("dots");
+  if (n > slides.length) {
+    slidePosition = 1;
+  }
+  if (n < 1) {
+    slidePosition = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < circles.length; i++) {
+    circles[i].className = circles[i].className.replace(" enable", "");
+  }
+  slides[slidePosition - 1].style.display = "block";
+  circles[slidePosition - 1].className += " enable";
+}
+
+//
+
+const testimonials = document.querySelectorAll(".testimonial");
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+
+let currentIndex = 0;
+
+function showTestimonial(index) {
+  testimonials.forEach((testimonial, i) => {
+    if (i === index) {
+      testimonial.style.opacity = 1;
+    } else {
+      testimonial.style.opacity = 0;
+    }
+  });
+}
+
+function nextTestimonial() {
+  currentIndex = (currentIndex + 1) % testimonials.length;
+  showTestimonial(currentIndex);
+}
+
+function prevTestimonial() {
+  currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
+  showTestimonial(currentIndex);
+}
+
+nextButton.addEventListener("click", nextTestimonial);
+prevButton.addEventListener("click", prevTestimonial);
+
+// Initially, show the first testimonial
+showTestimonial(currentIndex);
