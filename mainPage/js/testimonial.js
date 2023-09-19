d6357b7088d859fca18a0a88ae977145d27e7fd7
elinanomade <elinanomad@gmail.com>
2023-09-19 16:38:23 +0200

    landing page

---
+++ mainPage/js/testimonial.js
@@ -0,0 +1,33 @@
+"use script";
+
+const testimonials = document.querySelectorAll(".testimonial");
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+
+let currentIndex = 0;
+
+function showTestimonial(index) {
+  testimonials.forEach((testimonial, i) => {
+    if (i === index) {
+      testimonial.style.opacity = 1;
+    } else {
+      testimonial.style.opacity = 0;
+    }
+  });
+}
+
+function nextTestimonial() {
+  currentIndex = (currentIndex + 1) % testimonials.length;
+  showTestimonial(currentIndex);
+}
+
+function prevTestimonial() {
+  currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
+  showTestimonial(currentIndex);
+}
+
+nextButton.addEventListener("click", nextTestimonial);
+prevButton.addEventListener("click", prevTestimonial);
+
+// Initially, show the first testimonial
+showTestimonial(currentIndex);
--- mainPage/js/zodiac.js
+++ mainPage/js/zodiac.js
@@ -1,83 +1,79 @@
-//zodiac project: use an array of objects and attributes:
-"use strict";
-
 console.log("start here");
+
 var zodiac = [
   {
     zodiacName: "aries",
     birthdayRange: "March 21 - April 19",
     attributes:
-      "Aries, you are a crazy one! You feel at and shine your best if you are active, always a busy person. You are energetic, you will jump into things without thinking, you are the optimist, but on your worst you can be an aggressive, not so diplomatic. And one of your SUN CODE activities is: Listen a very heavy music.",
+      "Aries, you are a crazy one! You feel at and shine your best if you are active, always a busy person. You are energetic, you will jump into things without thinking, you are the optimist, but on your worst you can be an aggressive, not so diplomatic. And one of your SUN CODE activities is: Listen to very heavy music.",
   },
   {
     zodiacName: "cancer",
     birthdayRange: "June 21 - July 22",
     attributes:
-      "Many see you as the softest, but you are actually a warrior. You are kind and gentle at your best, but at your worst you can be a very nasty human who don't forget small things. What is one of your SUN CODE activities? Take care of plants - be a plant parent.",
+      "Many see you as the softest, but you are actually a warrior. You are kind and gentle at your best, but at your worst, you can be a very nasty human who doesn't forget small things. What is one of your SUN CODE activities? Take care of plants - be a plant parent.",
   },
   {
     zodiacName: "taurus",
     birthdayRange: "April 20 - May 20",
     attributes:
-      "Oh taurus, you hide so well behind that tough guy image! Financially you are the smartest from all, but at your low energy you and money are the enemies. What is one of your SUN CODE activities? Do some crafts!",
+      "Oh Taurus, you hide so well behind that tough guy image! Financially, you are the smartest of them all, but at your low energy, you and money are enemies. What is one of your SUN CODE activities? Do some crafts!",
   },
   {
     zodiacName: "gemini",
     birthdayRange: "May 21 - June 20",
     attributes:
-      "Dearest Gemini, wait, don't run away yet! Focus here! You are the communicator of the all signs, but at your lowest you can be shallow. What is one of your SUN CODE activities? Go cycle or get the drivers licence!",
+      "Dearest Gemini, wait, don't run away yet! Focus here! You are the communicator of all signs, but at your lowest, you can be shallow. What is one of your SUN CODE activities? Go cycling or get a driver's license!",
   },
   {
     zodiacName: "capricorn",
     birthdayRange: "December 22 - January 19",
     attributes:
-      "You are the climber of mountains and career! You shine at your best when you are disciplined, strict to yourself and have goals, but at your lowest you trying to reach the sky, makes you to become a workaholic. What is one of your SUN CODE activities? Go hiking!",
+      "You are the climber of mountains and career! You shine at your best when you are disciplined, strict with yourself and have goals, but at your lowest, trying to reach the sky makes you become a workaholic. What is one of your SUN CODE activities? Go hiking!",
   },
   {
     zodiacName: "sagittarius",
     birthdayRange: "November 22 - December 21",
     attributes:
-      "Sagittarius, world without you would be boring. You know how to travel and explore. You are cool, but at your low energy you are too arrogant. What is one of your SUN CODE activities? Go more often on roadtrips or learn a language!",
+      "Sagittarius, the world without you would be boring. You know how to travel and explore. You are cool, but at your low energy, you are too arrogant. What is one of your SUN CODE activities? Go on road trips more often or learn a language!",
   },
   {
     zodiacName: "pisces",
     birthdayRange: "February 19 - March 20",
     attributes:
-      "Hello the deep waters! You have the most complicated sign because you are a little bit of them all. At your best you are empathic human being, but at your worst you play the victim role. What is one of your SUN CODE activities? Go more out in nature and alone.",
+      "Hello, the deep waters! You have the most complicated sign because you are a little bit of them all. At your best, you are an empathic human being, but at your worst, you play the victim role. What is one of your SUN CODE activities? Spend more time in nature and alone.",
   },
   {
     zodiacName: "aquarius",
     birthdayRange: "January 20 - February 18",
     attributes:
-      "Hello there Miss Little Social Butterfly. Do you ever sleep? Well, you are a collective person, you love to talk and be with people. At your best you are friendly, but at your lowest the terrible weirdo. What is one of your SUN CODE activities? Few times in a month have a day without your phone!",
+      "Hello there, Miss Little Social Butterfly. Do you ever sleep? Well, you are a collective person, you love to talk and be with people. At your best, you are friendly, but at your lowest, you can be a terrible weirdo. What is one of your SUN CODE activities? Have a day without your phone a few times a month!",
   },
   {
     zodiacName: "scorpio",
     birthdayRange: "October 23 - November 21",
     attributes:
-      "Oh the mysterious human. You have the power to rule and be the king or queen. At your best you can lead and be a transformative leader, but at your worst you are brutal and manipulate with people. What is one of your SUN CODE activities? Do some extreme sport activities.",
+      "Oh, the mysterious human. You have the power to rule and be the king or queen. At your best, you can lead and be a transformative leader, but at your worst, you are brutal and manipulate people. What is one of your SUN CODE activities? Do some extreme sports activities.",
   },
   {
     zodiacName: "virgo",
     birthdayRange: "August 23 - September 22",
     attributes:
-      "The perfectionist of them all! Well, yes, everyone needs a Virgo in their life because you can structurize everything the best. At your best you are disciplened at work, but at the worst nasty pedant. What is one of your SUN CODE activities? Go to a SPA and have a massage.",
+      "The perfectionist of them all! Well, yes, everyone needs a Virgo in their life because you can structure everything the best. At your best, you are disciplined at work, but at your worst, you can be a nasty pedant. What is one of your SUN CODE activities? Go to a spa and have a massage.",
   },
   {
     zodiacName: "libra",
     birthdayRange: "September 23 - October 22",
     attributes:
-      "Hello fashionista. Yup, sort of fashion is related most to you. You are fashionable, friendly and have a beautiful aesthetic taste, but if you are at your low energies you can become snobby and codepenent. What is one of your SUN CODE activities? Go to museums! ",
+      "Hello, fashionista. Yup, sort of fashion is related most to you. You are fashionable, friendly, and have a beautiful aesthetic taste, but at your low energies, you can become snobby and codependent. What is one of your SUN CODE activities? Go to museums!",
   },
   {
     zodiacName: "leo",
     birthdayRange: "July 23 - August 22",
     attributes:
-      "The QUEEN/KING of Zodiac circle is here! Hi your Majesty, you are funny, playful and unique, but you can also step into negative side of your sign and become 'too much' type person. What is one of your SUN CODE activities? Have a professional photosession for yourself!",
+      "The QUEEN/KING of the Zodiac circle is here! Hi, your Majesty, you are funny, playful, and unique, but you can also step into the negative side of your sign and become the 'too much' type of person. What is one of your SUN CODE activities? Have a professional photoshoot for yourself!",
   },
 ];
-
-// write a function to call appropriate zodiacName:
 function astSign() {
   console.log("working");
 
@@ -86,20 +82,18 @@ function astSign() {
   var sign = document.getElementById("yourSign");
   var range = document.getElementById("birthdayRange");
   var attributes = document.getElementById("attributes");
-  var img = document.getElementById("zodiacImage");
 
-  for (i = 0; i < zodiac.length; i++) {
+  for (var i = 0; i < zodiac.length; i++) {
     if (userInput == zodiac[i].zodiacName) {
       sign.innerHTML = zodiac[i].zodiacName;
-
       range.innerHTML = zodiac[i].birthdayRange;
       attributes.innerHTML = zodiac[i].attributes;
-      img.src = zodiac[i].picture;
       return;
-    } else {
-      sign.innerHTML = "Oops! Check your English skills! Try again";
-      range.innerHTML = " ";
-      attributes.innerHTML = " ";
     }
   }
+
+  // If the user enters an invalid zodiac sign, display an error message.
+  sign.innerHTML = "Oops! Check your English skills! Try again";
+  range.innerHTML = "";
+  attributes.innerHTML = "";
 }
