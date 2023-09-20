08f29c972c2008b78a5704e2007ac9ddb8286492
elinanomade <elinanomad@gmail.com>
2023-09-20 19:02:08 +0200

    changes

--- mainPage/js/main.js
+++ mainPage/js/main.js
@@ -1,68 +1,17 @@
 "use strict";
 
-var slidePosition = 1;
-SlideShow(slidePosition);
+let prevScrollPos = window.pageYOffset;
 
-// forward/Back controls
-function plusSlides(n) {
-  SlideShow((slidePosition += n));
-}
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
 
-//  images controls
-function currentSlide(n) {
-  SlideShow((slidePosition = n));
-}
-
-function SlideShow(n) {
-  var i;
-  var slides = document.getElementsByClassName("Containers");
-  var circles = document.getElementsByClassName("dots");
-  if (n > slides.length) {
-    slidePosition = 1;
+  if (prevScrollPos > currentScrollPos) {
+    // User is scrolling up, show the header
+    document.querySelector("header").style.top = "0";
+  } else {
+    // User is scrolling down, hide the header
+    document.querySelector("header").style.top = "-100px"; // Adjust this value as needed
   }
-  if (n < 1) {
-    slidePosition = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < circles.length; i++) {
-    circles[i].className = circles[i].className.replace(" enable", "");
-  }
-  slides[slidePosition - 1].style.display = "block";
-  circles[slidePosition - 1].className += " enable";
-}
 
-//
-
-const testimonials = document.querySelectorAll(".testimonial");
-const prevButton = document.getElementById("prev");
-const nextButton = document.getElementById("next");
-
-let currentIndex = 0;
-
-function showTestimonial(index) {
-  testimonials.forEach((testimonial, i) => {
-    if (i === index) {
-      testimonial.style.opacity = 1;
-    } else {
-      testimonial.style.opacity = 0;
-    }
-  });
-}
-
-function nextTestimonial() {
-  currentIndex = (currentIndex + 1) % testimonials.length;
-  showTestimonial(currentIndex);
-}
-
-function prevTestimonial() {
-  currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
-  showTestimonial(currentIndex);
-}
-
-nextButton.addEventListener("click", nextTestimonial);
-prevButton.addEventListener("click", prevTestimonial);
-
-// Initially, show the first testimonial
-showTestimonial(currentIndex);
+  prevScrollPos = currentScrollPos;
+};
--- mainPage/js/newsletter.js
+++ mainPage/js/newsletter.js
@@ -1,6 +1,29 @@
 "use strict";
-var form = document.querySelector("form");
 
-form.onsubmit = function (e) {
+var form = document.getElementById("subscribe-form");
+var emailInput = document.getElementById("email-input");
+var confirmationMessage = document.getElementById("confirmation-message");
+
+form.addEventListener("submit", function (e) {
   e.preventDefault();
-};
+
+  // Get the entered email
+  var email = emailInput.value;
+
+  // Simulate saving the email to a database (replace with your actual database code)
+  // This is a placeholder for demonstration purposes
+  saveEmailToDatabase(email);
+
+  // Show the confirmation message
+  confirmationMessage.textContent = `Thank you for subscribing with email: ${email}`;
+  confirmationMessage.style.display = "block";
+
+  // Clear the email input
+  emailInput.value = "";
+});
+
+function saveEmailToDatabase(email) {
+  // In a real application, you would send the email to your server and save it in a database.
+  // Here, we're just logging it to the console as a placeholder.
+  console.log(`Saving email to database: ${email}`);
+}
