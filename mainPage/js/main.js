9d19ceddfc97e699726327e2574d9baca6a62d88
elinanomade <elinanomad@gmail.com>
2023-09-20 11:18:12 +0200

    button changes

--- mainPage/js/main.js
+++ mainPage/js/main.js
@@ -1,22 +1,3 @@
-// const prevButton = document.querySelector(".prev-button");
-// const nextButton = document.querySelector(".next-button");
-// const carouselItems = document.querySelector(".carousel-items");
-// const carouselItemWidth = document.querySelector(".carousel-item").clientWidth;
-// let currentPosition = 0;
-
-// prevButton.addEventListener("click", () => {
-//   if (currentPosition < 0) {
-//     currentPosition += carouselItemWidth;
-//     carouselItems.style.transform = `translateX(${currentPosition}px)`;
-//   }
-// });
-
-// nextButton.addEventListener("click", () => {
-//   if (currentPosition > -carouselItemWidth * 7) {
-//     currentPosition -= carouselItemWidth;
-//     carouselItems.style.transform = `translateX(${currentPosition}px)`;
-//   }
-// });
 "use strict";
 
 var slidePosition = 1;
---
+++ mainPage/js/newsletter.js
@@ -0,0 +1,6 @@
+"use strict";
+var form = document.querySelector("form");
+
+form.onsubmit = function (e) {
+  e.preventDefault();
+};
